1f390530f3365191488980597e12f4f6c38de5c6
aldi-f <aldi.filopati@cardoai.com>
2025-06-05 18:26:47 +0200

    converting prepare action to python

---
+++ prepare_configs/script.py
@@ -0,0 +1,75 @@
+import os
+import yaml
+import json
+from collections import namedtuple
+
+FOLDER_LIST = os.getenv('FOLDER_LIST', '').split(',')
+BRANCH = os.getenv('BRANCH', "")
+
+if not BRANCH:
+    raise ValueError("BRANCH environment variable is not set.")
+
+
+ALL_REPOS = []
+ALL_REPOS_NO_PLATFORMS = []
+
+Platform = namedtuple("Platform", ["os", "architecture"])
+
+for folder in FOLDER_LIST:
+    folder = folder.strip()
+    branch = BRANCH.replace('/', '_')
+
+    config = yaml.safe_load(f"./${folder}/config.yaml")
+
+    config_image = config.get('image', {})
+
+    tag = config_image.get('tag')
+    context = config_image.get('context', f"${folder}")
+    build_args = " ".join(config_image.get('build_args', []))
+
+    platforms = [
+        Platform(*platform.split('/'))
+        for platform in config_image.get('platforms', [])]
+
+
+    architectures = ",".join([platform.architecture for platform in platforms])
+
+
+    ecr_repositories = config.get('ecr_repositories', [])
+    
+    # filter only for current branch
+    ecr_repositories = [
+        repo for repo in ecr_repositories
+        if branch in repo.get('branch', [])
+    ]
+    
+    
+    for repo in ecr_repositories:, indent=0
+            'name': repo.get("name"),
+            "folder": folder,
+            "tag": tag,
+            "build_args": build_args,
+            "context": context,
+            "architecture": architectures,
+            "region": aws.get('region'),
+            "account_id": aws.get('account_id'),
+            "registry": f"${account_id}.dkr.ecr.${region}.amazonaws.com"
+        })
+        for platform in platforms:
+            ALL_REPOS.append({
+                'name': repo.get("name"),
+                "folder": folder,
+                "tag": tag,
+                "build_args": build_args,
+                "context": context,
+                "os": platform.os,
+                "architecture": platform.architecture,
+                "region": aws.get('region'),
+                "account_id": aws.get('account_id'),
+                "registry": f"${account_id}.dkr.ecr.${region}.amazonaws.com"
+            })
+    
+    
+with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+    f.write(f"ALL_REPOS={json.dumps(ALL_REPOS, indent=0)}\n")
+    f.write(f"ALL_REPOS_NO_PLATFORMS={json.dumps(ALL_REPOS_NO_PLATFORMS, indent=0)}\n")
