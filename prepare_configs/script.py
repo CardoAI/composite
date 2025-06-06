bf9dfd28716c5af8dbfcaeebab786448af564bf4
sdaberdaku <sebastian.daberdaku@cardoai.com>
2025-06-06 15:44:33 +0200

    refactor script.py to update build argument handling and clean up whitespace

--- prepare_configs/script.py
+++ prepare_configs/script.py
@@ -9,7 +9,6 @@ BRANCH = os.getenv('BRANCH', "")
 if not BRANCH:
     raise ValueError("BRANCH environment variable is not set.")
 
-
 ALL_REPOS = []
 ALL_REPOS_NO_PLATFORMS = []
 
@@ -20,23 +19,21 @@ for folder in FOLDER_LIST:
     with open(f"./{folder}/config.yaml", 'r') as f:
         config = yaml.safe_load(f)
     print(f"{config=}")
-    
+
     config_image = config.get('image', {})
 
     tag = config_image.get('tag')
     context = config_image.get('context', f"{folder}")
-    build_args = " ".join(config_image.get('build_args', []))
+    build_args = " ".join(config_image.get('extraArgs', []))
 
     platforms = [
         Platform(*platform.split('/'))
         for platform in config_image.get('platforms', [])]
 
-
     architectures = ",".join([platform.architecture for platform in platforms])
 
-
     ecr_repositories = config.get('ecrRepositories', [])
-    
+
     # filter only for current branch
     ecr_repositories = [
         repo for repo in ecr_repositories
@@ -78,6 +75,6 @@ for folder in FOLDER_LIST:
 with open(os.environ["GITHUB_OUTPUT"], "a") as f:
     print(f"ALL_REPOS={json.dumps(ALL_REPOS, indent=None)}", file=f)
     print(f"ALL_REPOS_NO_PLATFORMS={json.dumps(ALL_REPOS_NO_PLATFORMS, indent=None)}", file=f)
-    
+
 with open(os.environ["GITHUB_OUTPUT"], "r") as f:
     print(f.read())
